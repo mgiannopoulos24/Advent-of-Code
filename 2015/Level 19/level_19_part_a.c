17ec22ce0f7809f1d93929ee801db5cd5c2158e2
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-07-21 19:50:33 +0300

    Coded in C

--- 2015/Level 19/level_19_part_a.c
+++ 2015/Level 19/level_19_part_a.c
@@ -2,72 +2,105 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_REPLACEMENTS 1000
+#define MAX_LINE_LENGTH 100
+#define MAX_REPLACEMENTS 50
 #define MAX_MOLECULE_LENGTH 1000
 
 typedef struct {
-    char source[100];
-    char target[100];
+    char source[MAX_LINE_LENGTH];
+    char target[MAX_LINE_LENGTH];
 } Replacement;
 
-int generate_molecules(Replacement replacements[], char molecule[]) {
-    char new_molecule[MAX_MOLECULE_LENGTH];
-    int num_replacements = 0;
-    int molecule_length = strlen(molecule);
+typedef struct Node {
+    char molecule[MAX_MOLECULE_LENGTH];
+    struct Node *next;
+} Node;
 
-    for (int i = 0; i < molecule_length; i++) {
-        for (int j = 0; j < num_replacements; j++) {
-            int source_length = strlen(replacements[j].source);
-            if (strncmp(molecule + i, replacements[j].source, source_length) == 0) {
-                strcpy(new_molecule, molecule);
-                strcpy(new_molecule + i, replacements[j].target);
-                strcpy(new_molecule + i + strlen(replacements[j].target), molecule + i + source_length);
-                strcpy(molecule, new_molecule);
-                num_replacements++;
+void insert(Node **head, const char *molecule) {
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    strcpy(new_node->molecule, molecule);
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int contains(Node *head, const char *molecule) {
+    Node *current = head;
+    while (current != NULL) {
+        if (strcmp(current->molecule, molecule) == 0) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+void free_list(Node *head) {
+    Node *tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
+
+void generate_molecules(Replacement *replacements, int rep_count, const char *molecule, Node **molecules) {
+    int mol_len = strlen(molecule);
+
+    for (int i = 0; i < mol_len; i++) {
+        for (int j = 0; j < rep_count; j++) {
+            int src_len = strlen(replacements[j].source);
+            if (strncmp(&molecule[i], replacements[j].source, src_len) == 0) {
+                char new_molecule[MAX_MOLECULE_LENGTH];
+                strncpy(new_molecule, molecule, i);
+                new_molecule[i] = '\0';
+                strcat(new_molecule, replacements[j].target);
+                strcat(new_molecule, &molecule[i + src_len]);
+                if (!contains(*molecules, new_molecule)) {
+                    insert(molecules, new_molecule);
+                }
             }
         }
     }
-
-    return num_replacements;
 }
 
 int main() {
     FILE *file = fopen("input_level_19.txt", "r");
     if (file == NULL) {
-        printf("Error opening file.\n");
+        perror("Failed to open file");
         return 1;
     }
 
     Replacement replacements[MAX_REPLACEMENTS];
-    char molecule[MAX_MOLECULE_LENGTH];
-    int num_replacements = 0;
+    int rep_count = 0;
+    char line[MAX_LINE_LENGTH];
 
-    char line[100];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        if (line[strlen(line) - 1] == '\n') {
-            line[strlen(line) - 1] = '\0';
-        }
-
-        if (strcmp(line, "") == 0) {
+    while (fgets(line, sizeof(line), file)) {
+        if (strchr(line, '=') == NULL) {
             break;
         }
-
-        char *source = strtok(line, " ");
-        char *target = strtok(NULL, " ");
-        strcpy(replacements[num_replacements].source, source);
-        strcpy(replacements[num_replacements].target, target);
-        num_replacements++;
+        sscanf(line, "%s => %s", replacements[rep_count].source, replacements[rep_count].target);
+        rep_count++;
     }
 
+    char molecule[MAX_MOLECULE_LENGTH];
     fgets(molecule, sizeof(molecule), file);
-    if (molecule[strlen(molecule) - 1] == '\n') {
-        molecule[strlen(molecule) - 1] = '\0';
-    }
+    molecule[strcspn(molecule, "\n")] = 0;  // Remove newline character
 
     fclose(file);
 
-    int distinct_molecules = generate_molecules(replacements, molecule);
-    printf("%d\n", distinct_molecules);
+    Node *molecules = NULL;
+    generate_molecules(replacements, rep_count, molecule, &molecules);
+
+    int distinct_molecules_count = 0;
+    Node *current = molecules;
+    while (current != NULL) {
+        distinct_molecules_count++;
+        current = current->next;
+    }
+
+    printf("Number of distinct molecules: %d\n", distinct_molecules_count);
+
+    free_list(molecules);
 
     return 0;
 }
